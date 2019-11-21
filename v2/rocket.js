0c6317d12bec04459a30878f93e52211b848aab1
Mateus Mourão <mmdfmateus@gmail.com>
2019-11-20 23:45:07 -0300

    add colors to rockets

--- v2/rocket.js
+++ v2/rocket.js
@@ -82,7 +82,14 @@ function Rocket(dna) {
         push();
         //color customization of rockets
         noStroke();
-        fill(255, 150);
+
+        if(this.completed){
+          fill(0, 255, 0);
+        } else if(this.crashed){
+          fill(255, 0, 0);
+        } else{
+          fill(255, 150);
+        }
         //translate to the postion of rocket
         translate(this.pos.x, this.pos.y);
         //rotatates to the angle the rocket is pointing
