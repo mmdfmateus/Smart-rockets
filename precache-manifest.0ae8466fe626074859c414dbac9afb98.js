d4e370b4d001c580e089d4268ac1efbd0f45f82b
mateus <mmdfmateus@gmail.com>
2019-12-28 14:07:07 -0300

    Updates

---
+++ precache-manifest.0ae8466fe626074859c414dbac9afb98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f971947fc3e02bbb7724ecf7a349e3",
+    "url": "/Smart-rockets/index.html"
+  },
+  {
+    "revision": "42a2f2208a9f0a205b02",
+    "url": "/Smart-rockets/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "63fb207b5ccb7ce9d520",
+    "url": "/Smart-rockets/static/js/2.462ab8eb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Smart-rockets/static/js/2.462ab8eb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "42a2f2208a9f0a205b02",
+    "url": "/Smart-rockets/static/js/main.7673043d.chunk.js"
+  },
+  {
+    "revision": "c63455577cf463f0aea5",
+    "url": "/Smart-rockets/static/js/runtime-main.d5cd12cc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Smart-rockets/static/media/logo.5d5d9eef.svg"
+  }
+]);
